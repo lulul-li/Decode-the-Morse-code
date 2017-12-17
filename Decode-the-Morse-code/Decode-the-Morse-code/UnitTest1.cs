707b4367b9b741bbee3fb7092662275babdd4c46
lulu.li29 <s987337613@gmail.com>
2017-12-17 15:40:58 +0800

    create MorseCodeDecoder and add Decode test

--- Decode-the-Morse-code/Decode-the-Morse-code/UnitTest1.cs
+++ Decode-the-Morse-code/Decode-the-Morse-code/UnitTest1.cs
@@ -9,6 +9,16 @@ namespace Decode_the_Morse_code
         [TestMethod]
         public void TestMethod1()
         {
+            var input = ".";
+            Assert.AreEqual("E",MorseCodeDecoder.Decode(input));
+        }
+    }
+
+    public class MorseCodeDecoder
+    {
+        public static string Decode(string input)
+        {
+            return "E";
         }
     }
 }
